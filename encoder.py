873a5733f010516e9949df4c5e72eb400bae7cdd
paxF3E <pallav.sdsaxena2638@gmail.com>
2023-01-01 01:43:55 +0530

    skipthrough encoding managed by chunking

--- encoder.py
+++ encoder.py
@@ -4,38 +4,33 @@ import skipthoughts
 import os.path
 import pickle
 
-annotation_dir = 'Data/annotations'
+annotation_dir = 'Data/annotations/encodable'
 encoded_vector_dir = 'Data/encoded_vector'
 # garbage = 'Data/test_garbage'
 
 model = skipthoughts.load_model()
 onlyfiles = [f_ for f_ in listdir(annotation_dir) if isfile(join(annotation_dir, f_))]
-
-# onlyfiles_1 = onlyfiles[0:len(onlyfiles)//5]
-# onlyfiles_2 = onlyfiles[len(onlyfiles)//5:2*len(onlyfiles)//5]
-# onlyfiles_3 = onlyfiles[2*len(onlyfiles)//5:3*len(onlyfiles)//5]
-# onlyfiles_4 = onlyfiles[3*len(onlyfiles)//5:4*len(onlyfiles)//5]
-# onlyfiles_5 = onlyfiles[4*len(onlyfiles)//5:5*len(onlyfiles)//5]
-# for files = "Data/annotations/instances_val2017.json":
-files = "captions_train2017.json"
-files_ = files[0:-4]+'pkl'
-print(files_)
-if not os.path.exists(join(encoded_vector_dir,files_)):
-    with open(join(annotation_dir,files)) as f:
-        print(join(annotation_dir,files))
-        captions = f.read().split(',')
-        f.close()
-    captions = [cap for cap in captions if len(cap.strip()) > 0]
-    
-    try:
-        caption_vectors = skipthoughts.encode(model, captions)
-    except:
-        # with open(join(garbage,files), mode='w') as myfile:
-        #     myfile.write(' ')
-        pass
+splitfs = [file for file in onlyfiles if 'xa' in file] # splitting 100MB json into chunks and encode and dump using *split*
+splitfs.sort()
+for files in splitfs:
+    files_ = files+'.pkl' #files[0:-4]+'pkl'
     print(files_)
-    with open(join(encoded_vector_dir,files_), mode='wb+') as myfile:
-        pickle.dump(caption_vectors, myfile)
-        myfile.close()
-else:
-    print("skipped")
+    if not os.path.exists(join(encoded_vector_dir,files_)):
+        with open(join(annotation_dir,files)) as f:
+            print(join(annotation_dir,files))
+            captions = f.read().split(',')
+            f.close()
+        captions = [cap for cap in captions if len(cap.strip()) > 0]
+
+        try:
+            caption_vectors = skipthoughts.encode(model, captions)
+        except:
+            # with open(join(garbage,files), mode='w') as myfile:
+            #     myfile.write(' ')
+            pass
+        print(files_)
+        with open(join(encoded_vector_dir,"annotations_capt_train.pkl"), mode='ab+') as myfile:
+            pickle.dump(caption_vectors, myfile)
+            myfile.close()
+    else:
+        print("skipped")
--- prelim.py
+++ prelim.py
@@ -1,39 +1,49 @@
-import pandas as pd
-import os
-from pycocotools.coco import COCO
-import skimage.io as io
-import matplotlib.pyplot as plt
-from pathlib import Path
+# import pandas as pd
+# import os
+# from pycocotools.coco import COCO
+# import skimage.io as io
+# import matplotlib.pyplot as plt
+# from pathlib import Path
 
-dataDir = Path('Data/val2017')
-annFile = Path('Data/annotations/captions_val2017.json')
-cap = "A baker is working in the kitchen rolling dough."
-coco = COCO(annFile)
-annIDs = coco.getAnnIds()
-anns = coco.loadAnns(annIDs)
+# dataDir = Path('Data/val2017')
+# annFile = Path('Data/annotations/captions_val2017.json')
+# cap = "A baker is working in the kitchen rolling dough."
+# coco = COCO(annFile)
+# annIDs = coco.getAnnIds()
+# anns = coco.loadAnns(annIDs)
 
-for ann in anns:
-    if ann['caption'] == cap:
-        imgID = ann['image_id']
+# for ann in anns:
+#     if ann['caption'] == cap:
+#         imgID = ann['image_id']
 
 
-# imgIds = coco.getImgIds()
-# imgs = coco.loadImgs(imgIds[0])
+# # imgIds = coco.getImgIds()
+# # imgs = coco.loadImgs(imgIds[0])
 
-fig, axs = plt.subplots()
+# fig, axs = plt.subplots()
 
-## captions/anns mapped to given image
-# I = io.imread(dataDir/imgs[0]['file_name'])
-# annIds = coco.getAnnIds(imgIds=[imgs[0]['id']])
-# anns = coco.loadAnns(annIds)
+# ## captions/anns mapped to given image
+# # I = io.imread(dataDir/imgs[0]['file_name'])
+# # annIds = coco.getAnnIds(imgIds=[imgs[0]['id']])
+# # anns = coco.loadAnns(annIds)
+# # axs.imshow(I)
+# # plt.sca(axs)
+# # coco.showAnns(anns, draw_bbox=False)
+
+# ## image mapped to given caption
+# img = coco.loadImgs(imgID)
+# I = io.imread(dataDir/img[0]['file_name'])
 # axs.imshow(I)
 # plt.sca(axs)
-# coco.showAnns(anns, draw_bbox=False)
 
-## image mapped to given caption
-img = coco.loadImgs(imgID)
-I = io.imread(dataDir/img[0]['file_name'])
-axs.imshow(I)
-plt.sca(axs)
+# plt.show()
 
-plt.show()
+from pprint import pprint
+import json
+json_data=open("Data/annotations/annotations.json")
+jdata = json.load(json_data)
+
+print(type(jdata))
+# for key, value in jdata.values():
+#    pprint("Key:")
+#    pprint(key)
