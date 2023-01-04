e9e5dad9088d7fe6449d863e204f3500770531d5
paxF3E <pallav.sdsaxena2638@gmail.com>
2023-01-04 15:45:38 +0530

    trained on MNIST

--- tfimp1.py
+++ tfimp1.py
@@ -2,13 +2,12 @@ import tensorflow as tf
 import glob
 import imageio
 import matplotlib.pyplot as plt
-import numpy as np
 import os
-import PIL
+from PIL import Image
 from tensorflow.keras import layers
 import time
 from IPython import display
-print("\n\n\n\n\n")
+print("\n\n--------------------------------------------------------------\n\n")
 
 
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
@@ -17,6 +16,9 @@ train_images = (train_images - 127.5) / 127.5
 BUFFER_SIZE = 60000
 BATCH_SIZE = 256
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+print('''\n--------------------------------------------------------------------------------
+                                dataset loaded
+--------------------------------------------------------------------------------\n''')
 
 
 def generator_model():
@@ -42,11 +44,13 @@ def generator_model():
     assert model.output_shape == (None, 28, 28, 1)
     return model
 
-gen = generator_model()
+generator = generator_model()
+print('''\n--------------------------------------------------------------------------------
+                            generator instantiated
+--------------------------------------------------------------------------------\n''')
 noise = tf.random.normal([1, 100])
-generated_image = gen(noise, training=False)
-plt.imshow(generated_image[0, :, :, 0], cmap='gray')
-plt.show()
+generated_image = generator(noise, training=False)
+# plt.imshow(generated_image[0, :, :, 0], cmap='gray')
 
 
 def discriminator_model():
@@ -63,6 +67,105 @@ def discriminator_model():
     model.add(layers.Dense(1))
     return model
 
-disc = discriminator_model()
-decision = disc(generated_image)
-print(decision)
+discriminator = discriminator_model()
+print('''\n--------------------------------------------------------------------------------
+                            discriminator instantiated
+--------------------------------------------------------------------------------\n''')
+decision = discriminator(generated_image)
+print(f'''\n--------------------------------------------------------------------------------
+                            decision: {decision}
+--------------------------------------------------------------------------------\n''')
+
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+checkpoint_dir = 'training_checkpoints/'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+print('''\n--------------------------------------------------------------------------------
+                                checkpoint done
+--------------------------------------------------------------------------------\n''')
+
+
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+        for image_batch in dataset:
+            train_step(image_batch)
+        
+        display.clear_output(wait=True)
+        generate_and_save_images(generator, epoch + 1, seed)
+        if (epoch + 1) % 10 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+        
+        print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+        
+    display.clear_output(wait=True)
+    generate_and_save_images(generator, epochs, seed)
+
+def generate_and_save_images(model, epoch, test_input):
+    # all layers run in inference mode
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4, 4))
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+    plt.savefig('epoch_reports/image_at_epoch_{:04d}.png'.format(epoch))
+    plt.draw()
+    plt.show()
+
+print('''\n--------------------------------------------------------------------------------
+                                training initiated
+--------------------------------------------------------------------------------\n''')
+# train(train_dataset, EPOCHS)
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+def display_image(epoch_no): return Image.open('epoch_reports/image_at_epoch_{:04d}.png'.format(epoch_no))
+display_image(EPOCHS)
+gif_file = 'dcgan.gif'
+with imageio.get_writer(gif_file, mode='I') as writer:
+    filenames = glob.glob('image*.png')
+    filenames = sorted(filenames)
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    writer.close()
+
+# (Image.open(gif_file)).show()
